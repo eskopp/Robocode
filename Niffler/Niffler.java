ec828e7a157745be889450711bb8a6dbb1fb3b71
eskopp <skopp.erik@gmail.com>
2023-12-26 21:53:52 +0100

    Ausweichen

--- Niffler/Niffler.java
+++ Niffler/Niffler.java
@@ -26,8 +26,15 @@ public class Niffler extends Robot {
         turnRadarRight(radarTurn);
 
         // Turn the gun toward the scanned enemy and fire
-        turnGunRight(getHeading() + e.getBearing() - getGunHeading());
-        fire(2);
+        double gunTurn = getHeading() + e.getBearing() - getGunHeading();
+        turnGunRight(gunTurn);
+        
+        // If the enemy is close, increase firepower
+        if (e.getDistance() < 200) {
+            fire(3);
+        } else {
+            fire(1);
+        }
         scannedEnemy = true;
     }
 
@@ -41,8 +48,24 @@ public class Niffler extends Robot {
 
     // Method for dynamic robot movement
     private void performDynamicMovement() {
-        ahead(100);
-        back(100);
-        turnRight(45);
+        ahead(150); // Move forward a bit more
+
+        // If close to a wall, turn away from the wall
+        if (getX() < 50 || getX() > getBattleFieldWidth() - 50 || getY() < 50 || getY() > getBattleFieldHeight() - 50) {
+            back(50);
+            turnRight(90);
+        }
+
+        // Randomly change direction to avoid predictability
+        if (Math.random() < 0.05) {
+            turnRight(Math.random() * 90);
+        }
+    }
+
+    // Event handler for when the robot hits a wall
+    public void onHitWall(HitWallEvent e) {
+        // Turn away from the wall
+        back(20);
+        turnRight(90);
     }
 }
