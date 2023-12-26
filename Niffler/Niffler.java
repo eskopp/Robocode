cd2864dca430ba9c2dc5edde758fc25566c6e4a9
eskopp <skopp.erik@gmail.com>
2023-12-26 22:01:26 +0100

    Ausweichen

--- Niffler/Niffler.java
+++ Niffler/Niffler.java
@@ -28,7 +28,7 @@ public class Niffler extends Robot {
         // Turn the gun toward the scanned enemy and fire
         double gunTurn = getHeading() + e.getBearing() - getGunHeading();
         turnGunRight(gunTurn);
-        
+
         // If the enemy is close, increase firepower
         if (e.getDistance() < 200) {
             fire(3);
@@ -48,24 +48,16 @@ public class Niffler extends Robot {
 
     // Method for dynamic robot movement
     private void performDynamicMovement() {
-        ahead(150); // Move forward a bit more
-
-        // If close to a wall, turn away from the wall
-        if (getX() < 50 || getX() > getBattleFieldWidth() - 50 || getY() < 50 || getY() > getBattleFieldHeight() - 50) {
-            back(50);
-            turnRight(90);
-        }
+        // Move forward by default
+        ahead(100);
 
         // Randomly change direction to avoid predictability
-        if (Math.random() < 0.05) {
-            turnRight(Math.random() * 90);
+        if (Math.random() < 0.1) {
+            if (Math.random() < 0.5) {
+                turnRight(Math.random() * 90);
+            } else {
+                turnLeft(Math.random() * 90);
+            }
         }
-    }
-
-    // Event handler for when the robot hits a wall
-    public void onHitWall(HitWallEvent e) {
-        // Turn away from the wall
-        back(20);
-        turnRight(90);
     }
 }
--- basic.cmd
+++ basic.cmd
@@ -1,6 +1,6 @@
 @echo off
 cd Niffler
 javac -cp robocode.jar Niffler.java 
-copy Niffler.class ../Niffler.class
+copy /Y Niffler.class ../Niffler.class
 :: Basic installation
 copy /Y Niffler.class C:\robocode\robots\Niffler.class
