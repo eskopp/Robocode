3db9e167430359a41a02610f0b7b9c60f1cbcf75
eskopp <skopp.erik@gmail.com>
2023-12-26 21:41:11 +0100

    getRadarTurnRemaining

--- Niffler/Niffler.java
+++ Niffler/Niffler.java
@@ -2,24 +2,47 @@ import robocode.*;
 
 public class Niffler extends Robot {
 
+    // Flag to check if an enemy has been scanned
+    boolean scannedEnemy = false;
+
     public void run() {
-        // Setze den Roboternamen
-        // setName("Niffler"); // Entfernt, da der Robotername direkt in Robocode festgelegt wird
-    
-        // Setze die Farben des Roboters
+        // Set robot colors
         setColors(java.awt.Color.BLUE, java.awt.Color.YELLOW, java.awt.Color.RED, java.awt.Color.ORANGE, java.awt.Color.WHITE);
-    
-        // Setze die Radarsichtbarkeit
         setAdjustRadarForRobotTurn(true);
         setAdjustGunForRobotTurn(true);
-    
-        // Die Hauptprogrammschleife
+
+        // Main loop
         while (true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
+            // Scan for enemies and perform dynamic movement
+            scanForEnemies();
+            performDynamicMovement();
         }
     }
-    
+
+    // Event handler for when an enemy is scanned
+    public void onScannedRobot(ScannedRobotEvent e) {
+        // Turn the radar toward the scanned enemy
+        double radarTurn = getHeading() + e.getBearing() - getRadarHeading();
+        turnRadarRight(radarTurn);
+
+        // Turn the gun toward the scanned enemy and fire
+        turnGunRight(getHeading() + e.getBearing() - getGunHeading());
+        fire(2);
+        scannedEnemy = true;
+    }
+
+    // Method to scan for enemies continuously
+    private void scanForEnemies() {
+        if (!scannedEnemy) {
+            turnRadarRight(360);
+        }
+        scannedEnemy = false;
+    }
+
+    // Method for dynamic robot movement
+    private void performDynamicMovement() {
+        ahead(100);
+        back(100);
+        turnRight(45);
+    }
 }
